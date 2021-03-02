87258c4c22f73401b0501079b4f25977abd72665
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-03-02 19:08:01 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -10,7 +10,10 @@ bool isValueOutOfRange (float value, float min, float max)
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) 
 {
-  bool tempCheck = isValueOutOfRange(temperature , 0, 45);
+  float tempr_min = 0.0F, tempr_max = 45.0F;
+  float soc_min = 20.0F, soc_max = 80.0F;
+  float charge_rate_max = 0.8F;
+  bool tempCheck = isValueOutOfRange(temperature , tempr_min, tempr_max);
   bool socCheck = isValueOutOfRange(soc , 20, 80);
   bool batteryIsNotOkay = tempCheck || socCheck || chargeRate > 0.8;
   return !batteryIsNotOkay;
